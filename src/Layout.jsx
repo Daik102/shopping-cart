a1b90946485416a498ed522066ea3d26b5cd082c
Daik <202999281+Daik102@users.noreply.github.com>
2026-09-08 00:33:27 +0900

    Remove temporary buttons

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -41,14 +41,8 @@ function Layout() {
     );
   });
 
-  const a = {id: 1, title: "aaa", price: 10, image: "xxx"};
-  console.log(cart);
-
   return (
     <div>
-      <button className='btn' onClick={() => addToCart(a, 2)}>Add</button>
-      <button onClick={() => updateQuantity(1, 5)}>Update</button>
-      <button onClick={() => removeFromCart(1)}>Remove</button>
       <Navbar cart={cart} />
       <main>
         <Outlet context={{ cart, addToCart, updateQuantity, removeFromCart }} />
