b6aed49a5148a1f75f1b0053de040b79031a82b3
Daik <202999281+Daik102@users.noreply.github.com>
2026-09-08 00:34:47 +0900

    Add styling for Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,26 @@
 import { NavLink } from "react-router";
-//import "./Navbar.css";
+import styles from "./Navbar.module.css";
 
 function Navbar({ cart = 0 }) {
   const totalItemCount = cart.reduce((total, item) => total += item.quantity, 0);
 
   return (
     <nav>
-      <NavLink to="/" end>
+      <NavLink 
+        to="/" end
+        className={({ isActive }) => (isActive ? styles.customActive : styles.navItem)}
+      >
         Home
       </NavLink>
       <NavLink 
         to="/shop"
-        //className={({ isActive }) => (isActive ? "custom-active" : "nav-item")}
+        className={({ isActive }) => (isActive ? styles.customActive : styles.navItem)}
       >
         Shop
       </NavLink>
       <NavLink 
         to="/cart"
-        //className={({ isActive }) => (isActive ? "custom-active" : "nav-item")}
+        className={({ isActive }) => (isActive ? styles.customActive : styles.navItem)}
       >
         Cart {totalItemCount > 0 && <span>({totalItemCount})</span>}
       </NavLink>
