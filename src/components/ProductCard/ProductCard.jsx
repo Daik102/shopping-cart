4d270199264ec0b5272e464ee59a5c9d3f993fb3
Daik <202999281+Daik102@users.noreply.github.com>
2026-09-08 23:36:24 +0900

    Adjust file structure

---
+++ src/components/ProductCard/ProductCard.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react';
+import styles from './ProductCard.module.css';
+
+function ProductCard({ product, addToCart }) {
+  const [quantity, setQuantity] = useState(1);
+
+  const handleAddToCart = () => {
+    addToCart(product, Number(quantity));
+  };
+
+  return (
+    <li className={styles.product}>
+      <img className={styles.productImage} src={product.image} alt={product.title} />
+      <p className={styles.productTitle}>{product.title}</p>
+      <p className={styles.productPrice}>${product.price}</p>
+      <div className={styles.actionsContainer}>
+        <div className={styles.quantityContainer}>
+          <select 
+            id={`quantity-${product.id}`}
+            value={quantity} 
+            onChange={(e) => setQuantity(e.target.value)}
+            className={styles.quantitySelect}
+            aria-label="Select quantity"
+          >
+            {Array.from({ length: 10 }, (_, i) => i + 1).map((num) => (
+              <option key={num} value={num}>
+                Quantity: {num}
+              </option>
+            ))}
+          </select>
+        </div>
+        <button className={styles.addToCartBtn} onClick={handleAddToCart}>
+          Add to Cart
+        </button>
+      </div>
+    </li>
+  );
+}
+
+export default ProductCard
