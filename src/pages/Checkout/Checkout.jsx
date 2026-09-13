a22f00de8503375b9645dded3e3e42aa946cb3f9
Daik <202999281+Daik102@users.noreply.github.com>
2026-09-13 16:51:48 +0900

    Implement checkout page

--- src/pages/Checkout/Checkout.jsx
+++ src/pages/Checkout/Checkout.jsx
@@ -1,13 +1,20 @@
 import { Link } from 'react-router';
+import { Construction } from 'lucide-react';
 import styles from '../Cart/Cart.module.css';
 
 export function Checkout() {
   return (
     <div className={styles.wrapper}>
-      <h1 className={styles.cartTitle}>Sorry, this is a practice site</h1>
-      <Link to="/cart" className={styles.backToTheCartBtn}>
-        Back to the cart
-      </Link>
+      <div className={styles.checkoutContainer}>
+        <Construction size={64} className={styles.checkoutIcon} />
+        <h2 className={styles.checkoutTitle}>Sorry, this is a practice site</h2>
+        <p className={styles.checkoutText}>
+          Checkout feature might be added in the future. Stay tuned.
+        </p>
+        <Link to="/cart" className={styles.backToTheCartBtn}>
+          Back to the cart
+        </Link>
+      </div>
     </div>
   );
 }
