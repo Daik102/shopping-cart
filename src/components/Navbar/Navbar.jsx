0d1ef43f1045eae6fe30fe033d2ac9181adc064c
Daik <202999281+Daik102@users.noreply.github.com>
2026-09-13 21:03:04 +0900

    Add sparlke icon

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,17 +1,20 @@
 import { NavLink } from "react-router";
-import { House, Store, ShoppingCart } from 'lucide-react';
+import { Sparkle, House, Store, ShoppingCart } from 'lucide-react';
 import styles from "./Navbar.module.css";
 
 function Navbar({ cartCount }) {
   return (
     <nav>
-      <h1 className={styles.navbarTitle}>Odin Shopping Cart</h1>
+      <h1 className={styles.navbarTitle}>
+        <Sparkle className={styles.titleIcon} size={20} aria-hidden="true" />
+        Odin Shopping Cart
+      </h1>
       <NavLink 
         to="/" end
         className={({ isActive }) => (isActive ? styles.customActive : styles.navItem)}
       >
-        <span className={styles.cartLinkContent}>
-          <House className={styles.cartIcon} size={20} aria-hidden="true" />
+        <span className={styles.linkContent}>
+          <House className={styles.linkIcon} size={20} aria-hidden="true" />
           Home
         </span>
       </NavLink>
@@ -19,8 +22,8 @@ function Navbar({ cartCount }) {
         to="/shop"
         className={({ isActive }) => (isActive ? styles.customActive : styles.navItem)}
       >
-        <span className={styles.cartLinkContent}>
-          <Store className={styles.cartIcon} size={20} aria-hidden="true" />
+        <span className={styles.linkContent}>
+          <Store className={styles.linkIcon} size={20} aria-hidden="true" />
           Shop
         </span>
       </NavLink>
@@ -28,8 +31,8 @@ function Navbar({ cartCount }) {
         to="/cart"
         className={({ isActive }) => (isActive ? styles.customActive : styles.navItem)}
       >
-        <span className={styles.cartLinkContent}>
-          <ShoppingCart className={styles.cartIcon} size={20} aria-hidden="true" />
+        <span className={styles.linkContent}>
+          <ShoppingCart className={styles.linkIcon} size={20} aria-hidden="true" />
           Cart
           <span className={styles.badge}>({cartCount})</span>
         </span>
