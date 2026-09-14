ff3b62e10ef2a0872c23828427766ee8771df4c8
Daik <202999281+Daik102@users.noreply.github.com>
2026-09-14 15:38:32 +0900

    Update classNames

--- src/pages/Checkout/Checkout.jsx
+++ src/pages/Checkout/Checkout.jsx
@@ -1,18 +1,18 @@
 import { Link } from 'react-router';
 import { Construction } from 'lucide-react';
-import styles from '../Cart/Cart.module.css';
+import styles from '../../components/ErrorView/ErrorView.module.css';
 
 export function Checkout() {
   return (
     <div className={styles.wrapper}>
-      <div className={styles.checkoutContainer}>
-        <Construction size={64} className={styles.checkoutIcon} />
-        <h2 className={styles.checkoutTitle}>Sorry, this is a practice site</h2>
-        <p className={styles.checkoutText}>
+      <div className={styles.container}>
+        <Construction size={64} className={styles.icon} />
+        <h2 className={styles.title}>Sorry, this is a practice site</h2>
+        <p className={styles.text}>
           Checkout feature might be added in the future. Stay tuned.
         </p>
-        <Link to="/cart" className={styles.backToTheCartBtn}>
-          Back to the cart
+        <Link to="/cart" className={styles.actionBtn}>
+          Back to Cart
         </Link>
       </div>
     </div>
